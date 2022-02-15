b7d6e7a6fbc1c79ec97e6985434580f4b8195d1a
Olusola Afikode <afikodeolaitan@gmail.com>
2022-02-15 08:48:29 +0100

    Complete Homepage UI

---
+++ components/Layout.js
@@ -0,0 +1,46 @@
+import {
+  AppBar,
+  Container,
+  Link,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import Head from "next/head";
+import React from "react";
+import useStyles from "../utils/styles";
+import NextLink from "next/link";
+
+function Layout({ children, title }) {
+  const classes = useStyles();
+  return (
+    <div>
+      <Head>
+        <title>{title ? `${title} - Kwikii Store` : `Kwikii Store`}</title>
+      </Head>
+      <AppBar position="static" className={classes.navbar}>
+        <Toolbar>
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography className={classes.brand}>Kwikki</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href="/cart" passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href="/login" passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
+        <Typography>All rights reserved @kwikki store 2022</Typography>
+      </footer>
+    </div>
+  );
+}
+
+export default Layout;
